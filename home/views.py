77f839db37f4b6e9f1e1d18a87e4299b2f933351
oliviaspark0825 <suhyunpark0825@gmail.com>
2019-02-13 01:49:24 +0000

    02_welcome django

--- home/views.py
+++ home/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from pprint import pprint
 
 # Create your views here.
+def index(request):
+    print(request)
+    print(type(request))
+    pprint(request.META)
+    return HttpResponse('goed morgen!')

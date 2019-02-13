a459f88b8b5e7351cebd6b25544f569761821132
oliviaspark0825 <suhyunpark0825@gmail.com>
2019-02-13 02:27:55 +0000

    03_template variable

--- django_intro/urls.py
+++ django_intro/urls.py
@@ -19,6 +19,7 @@ from home import views
 
 
 urlpatterns = [
+    path('home/dinner/', views.dinner, name='dinner'),
     path('home/index/', views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
--- home/views.py
+++ home/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render, HttpResponse
 from pprint import pprint
+import random
 
 # Create your views here.
 def index(request):
-    print(request)
-    print(type(request))
-    pprint(request.META)
+    # print(request)
+    # print(type(request))
+    # pprint(request.META)
     return HttpResponse('goed morgen!')
+    
+def dinner(request):
+    menus = ['닭볶음탕', '치킨', '소고기']
+    # return HttpResponse(random.choice(menu))
+    pick = random.choice(menus)
+    return render(request, 'dinner.html', {'menus': menus, 'pick': pick})
